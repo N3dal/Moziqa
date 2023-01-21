a8ab69f6745b0a2821366cc150ec37bd9ccd3557
N3dal <nedalxzo@gmail.com>
2023-01-21 21:33:23 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # -----------------------------------------------------------------
 # This is one of the 50 python projects
 # GUI music player, simple one only start and pause the song.
@@ -27,17 +28,29 @@ system("clear")
 
 class MainWindow(QMainWindow):
     """"""
-    
-    
+
+    WIDTH = 380
+    HEIGHT = 600
+    TITLE = "Moziqa"
+
+    STYLESHEET = """
+        
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
-        
+
+        self.setFixedSize(MainWindow.WIDTH, MainWindow.HEIGHT)
+        self.setWindowTitle(MainWindow.TITLE)
 
 
 def main():
 
     app = QApplication(sys.argv)
+
+    root = MainWindow()
+
+    root.show()
 
     sys.exit(app.exec())
 

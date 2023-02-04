3062a000a8fc70ffaf1a491c88aa5a963b75df0f
N3dal <nedalxzo@gmail.com>
2023-02-04 15:25:53 +0200

    created the main frame and added new buttons to title frame

--- main.py
+++ main.py
@@ -50,6 +50,16 @@ class TitleBar(QFrame):
         font-family: Mono;
     """
 
+    BUTTON_STYLESHEET = """
+        QPushButton{
+            border-radius: 5px;
+        }
+        
+        QPushButton:hover{
+            background-color: #d8e1ee;
+        }
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -57,54 +67,58 @@ class TitleBar(QFrame):
 
         self.setStyleSheet(TitleBar.STYLESHEET)
 
-        self.title_label = QLabel(
-            parent=self, text=f"<h3>{defaults.TITLE}</h3>")
-
         self.old_mouse_position = QPoint()
 
-        # create the title label;
-        self.title_label.setStyleSheet(TitleBar.LABEL_STYLESHEET)
+        # create the menu button;
+        self.menu_btn = QPushButton(parent=self)
+        self.menu_btn.setFixedSize(34, 34)
+        self.menu_btn.setIcon(QIcon("./assets/pictures/menu.png"))
+        self.menu_btn.setIconSize(QSize(24, 24))
+        # self.menu_btn.clicked.connect(self.parent().showMinimized)
+        self.menu_btn.setCursor(QCursor(Qt.PointingHandCursor))
+        self.menu_btn.setStyleSheet(TitleBar.BUTTON_STYLESHEET)
+        self.menu_btn.move(0, 0)
 
-        self.title_label.move(20, 5)
-
+        # create the icon;
         self.icon = QPushButton(parent=self)
-
         self.icon.setIcon(QIcon("./assets/pictures/icon.png"))
-
-        self.icon.setIconSize(QSize(32, 32))
-
-        self.icon.clicked.connect(self.parent().showMinimized)
-
+        self.icon.setIconSize(QSize(24, 24))
+        # self.icon.clicked.connect(self.parent().showMinimized)
         self.icon.setCursor(QCursor(Qt.PointingHandCursor))
+        self.icon.move(45, 3)
 
-        self.icon.move(280, 3)
+        # create the title label;
+        self.title_label = QLabel(
+            parent=self, text=f"<h3>{defaults.TITLE}</h3>")
+        self.title_label.setStyleSheet(TitleBar.LABEL_STYLESHEET)
+        self.title_label.move(80, 7)
 
         # create the close button;
         self.close_btn = QPushButton(parent=self)
-
         self.close_btn.setIcon(QIcon("./assets/pictures/close.png"))
-
         self.close_btn.setIconSize(QSize(24, 24))
-
         self.close_btn.clicked.connect(sys.exit)
-
         self.close_btn.setCursor(QCursor(Qt.PointingHandCursor))
-
-        self.close_btn.move(318, 3)
+        self.close_btn.setStyleSheet(TitleBar.BUTTON_STYLESHEET)
+        self.close_btn.move(self.width() - 30, 5)
 
         # create the minimize button;
-
         self.minimize_btn = QPushButton(parent=self)
-
         self.minimize_btn.setIcon(QIcon("./assets/pictures/minimize.png"))
-
         self.minimize_btn.setIconSize(QSize(24, 24))
-
         self.minimize_btn.clicked.connect(self.parent().showMinimized)
-
         self.minimize_btn.setCursor(QCursor(Qt.PointingHandCursor))
+        self.minimize_btn.setStyleSheet(TitleBar.BUTTON_STYLESHEET)
+        self.minimize_btn.move(self.width() - 60, 5)
 
-        self.minimize_btn.move(280, 3)
+        # create the more-option button;
+        self.more_option_btn = QPushButton(parent=self)
+        self.more_option_btn.setIcon(QIcon("./assets/pictures/options.png"))
+        self.more_option_btn.setIconSize(QSize(24, 24))
+        # self.more_option_btn.clicked.connect(self.parent().showMinimized)
+        self.more_option_btn.setCursor(QCursor(Qt.PointingHandCursor))
+        self.more_option_btn.setStyleSheet(TitleBar.BUTTON_STYLESHEET)
+        self.more_option_btn.move(self.width() - 90, 5)
 
     def mousePressEvent(self, e):
 
@@ -130,6 +144,30 @@ class TitleBar(QFrame):
         return None
 
 
+class MainFrame(QFrame):
+    """
+        The main frame that will hold everything;
+
+    """
+
+    STYLESHEET = """
+        
+        background-color: #282828;
+        border-top-left-radius: 0px;
+        border-top-right-radius:0px;
+        border-bottom-left-radius: 10px;
+        border-bottom-right-radius: 10px;
+        
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.setFixedSize(self.parent().width(), self.parent().height() - 37)
+
+        self.setStyleSheet(MainFrame.STYLESHEET)
+
+
 class MainWindow(QMainWindow):
     """"""
 
@@ -150,10 +188,13 @@ class MainWindow(QMainWindow):
         self.setWindowFlags(Qt.FramelessWindowHint)
 
         # to make the main window transparent;
-        # self.setAttribute(Qt.WA_TranslucentBackground)
+        self.setAttribute(Qt.WA_TranslucentBackground)
 
         self.title_bar = TitleBar(parent=self)
         self.title_bar.move(0, 0)
+
+        self.main_frame = MainFrame(parent=self)
+        self.main_frame.move(0, TitleBar.HEIGHT)
 
 
 def main():
